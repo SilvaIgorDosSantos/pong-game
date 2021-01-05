828be98b97b9ae3fb49b320d080cdd0a1c50e85e
SilvaIgorDosSantos <silvaigordossantos@gmail.com>
2021-01-05 15:19:47 -0300

    Add ball class with constructor, reset, update and render methods

--- js/sketch.js
+++ js/sketch.js
@@ -1,5 +1,38 @@
+class Ball {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+
+    this.theta = random(0, 2*PI);
+    this.v = 2;
+  }
+
+  reset(x, y) {
+    this.x = x;
+    this.y = y;
+
+    this.theta = random(0, 2*PI);
+    this.v = 2;
+  }
+
+  render() {
+    rect(this.x, this.y, this.width, this.height);
+  }
+
+  update() {
+    this.vx = this.v * cos(this.theta);
+    this.vy = this.v * sin(this.theta);
+
+    this.x += this.vx;
+    this.y += this.vy;
+  }
+}
+
 let windowWidth, windowHeight;
 let gameWidth, gameHeight;
+let ball;
 
 function setup() {
     windowWidth = window.innerWidth;
@@ -12,8 +45,21 @@ function setup() {
 
     let canvas = createCanvas(gameWidth, gameHeight);
     canvas.position(windowWidth/2 - gameWidth/2,windowHeight/2 - gameHeight/2);
+
+    noStroke();
+    fill(255, 255, 255);
+
+    ball = new Ball(gameWidth/2, gameHeight/2, 10, 10);
+
+
   }
   
   function draw() {
     background(0, 0, 0);
+
+    ball.update();
+
+    ball.render();
+
+    
   }
